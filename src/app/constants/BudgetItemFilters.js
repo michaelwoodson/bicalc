bf05f94f55c6902604dd6a93c99e17762d4bf643
Michael Woodson <mwoodson@fmyi.com>
2016-10-20 09:14:16 -0500

    Remove filters.

--- src/app/constants/BudgetItemFilters.js
+++
@@ -1,3 +0,0 @@
-export const SHOW_ALL = 'show_all';
-export const SHOW_APPLIED = 'show_applied';
-export const SHOW_AVAILABLE = 'show_available';
--- src/app/containers/App.vue
+++ src/app/containers/App.vue
@@ -16,7 +16,7 @@
         />
       <ul>
         <budget-item
-          v-for="budgetItem in budgetItems.filter(selectedFilter.filter)"
+          v-for="budgetItem in budgetItems"
           :budgetItem="budgetItem"
         ></budget-item>
       </ul>
@@ -24,11 +24,6 @@
         <span>
           <strong>{{availableCount || 'No'}}</strong> {{availableCount === 1 ? 'item' : 'items'}} left
         </span>
-        <ul class="filters">
-          <li v-for="filter in filters">
-            <a v-bind:style="{cursor: 'pointer'}" v-bind:class="{'selected': filter === selectedFilter.type}" v-on:click="onShow(filter)">{{filterTitles[filter]}}</a>
-          </li>
-        </ul>
         <h1>${{basicIncome | money}} / Month</h1>
       </footer>
     </section>
@@ -38,26 +33,12 @@
 <script>
 import {mapActions, mapGetters} from 'vuex';
 
-import VisibilityFilters from '../constants/VisibilityFilters';
-import {SHOW_ALL, SHOW_APPLIED, SHOW_AVAILABLE} from '../constants/BudgetItemFilters';
 import BudgetItem from '../components/BudgetItem.vue';
-
-const filterTitles = {
-  [SHOW_ALL]: 'All',
-  [SHOW_AVAILABLE]: 'Available',
-  [SHOW_APPLIED]: 'Applied'
-};
-
-const filters = [SHOW_ALL, SHOW_AVAILABLE, SHOW_APPLIED];
 
 export default {
   name: 'App',
   data() {
     return {
-      filterTitles,
-      filters,
-      filter: SHOW_ALL,
-      mainFilter: SHOW_ALL
     };
   },
   computed: {
@@ -74,9 +55,6 @@ export default {
     availableCount() {
       return this.budgetItems.length - this.appliedCount;
     },
-    selectedFilter() {
-      return VisibilityFilters[this.mainFilter];
-    },
     population () {
       return this.$store.state.population
     },
@@ -88,9 +66,6 @@ export default {
     ...mapActions(['applyAll']),
     handleApplyAll() {
       this.applyAll();
-    },
-    onShow(filter) {
-      this.mainFilter = filter;
     }
   },
   components: {
